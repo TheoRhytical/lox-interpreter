0ecb2210a75b07882676580fa58e739c43180737
TheoRhytical <rhyscabonita@gmail.com>
2024-02-05 11:06:16 +0800

    Parser error handling; Connected Parser to Interpreter

--- lox/Parser.java
+++ lox/Parser.java
@@ -5,11 +5,21 @@ import java.util.List;
 import static lox.TokenType.*;
 
 public class Parser {
+	private static class ParseError extends RuntimeException {}
+
 	private final List<Token> tokens;
 	private int current;
 
 	Parser(List<Token> tokens) {
 		this.tokens = tokens;
+	}
+
+	Expr parse() {
+		try {
+			return expression();
+		} catch (ParseError error) {
+			return null;
+		}
 	}
 
 	private Expr expression() {
@@ -87,10 +97,8 @@ public class Parser {
 			consume(RIGHT_PAREN, "Expect ')' after expression.");
 			return new Expr.Grouping(expr);
 		}
-	}
 
-	private void consume(TokenType type, String err) {
-		
+		throw error(peek(), "Expect expression.");
 	}
 
 	private boolean match(TokenType... types) {
@@ -101,6 +109,39 @@ public class Parser {
 			}
 		}
 		return false;
+	}
+
+	private Token consume(TokenType type, String message) {
+		if (check(type)) return advance();
+
+		throw error(peek(), message);
+	}
+
+	static ParseError error(Token token, String message) {
+		Lox.error(token, message);
+		return new ParseError();
+	}
+
+	private void synchronize() {
+		advance();
+
+		while (!isAtEnd()) {
+			if (previous().type == SEMICOLON) return;
+
+			switch (peek().type) {
+				case CLASS:
+				case FUN:
+				case VAR:
+				case FOR:
+				case IF:
+				case WHILE:
+				case PRINT:
+				case RETURN:
+					return;
+			}
+			
+			advance();
+		}
 	}
 
 	private Token advance() {
